813149aef5f12a3e028ba444692a9886decb351e
Damilare Durojaye <damilare.durojaye@gmail.com>
2019-08-13 12:51:04 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            text: 'A line of text in a paragraph.',
+          },
+        ],
+      },
+    ],
+  },
+})
+
+// Define our app...
+/* class App extends React.Component {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue,
+  }
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
+  // Render the editor.
+  render() {
+    return <Editor value={this.state.value} onChange={this.onChange} />
+  }
+} */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [value, setValue] = useState(initialValue);
+
+  return <Editor value={value} onChange={val => setValue(val.value)} />
 }
 
 export default App;
